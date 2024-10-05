4194d1a19c32e3905902736b5b49cdbc9af1b947
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-05 17:53:26 +0530

    Create Chocolate Distribution.cpp

---
+++ Arrays/Day2/Chocolate Distribution.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m)
+    {
+        sort(a.begin(),a.end());
+        long long mindiff = LLONG_MAX;
+        for(int i=0;i+m-1<n;i++)
+        {
+            mindiff = min(mindiff,a[i+m-1]-a[i]);
+        }
+        return mindiff;
+    }   
+};
